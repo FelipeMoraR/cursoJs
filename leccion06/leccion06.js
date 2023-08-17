3de637d9d6db7728d41d31725efc6230ec5ffcb9
Felipe Mora <gnar2342@gmail.com>
2023-08-17 13:51:39 -0400

    Advance
    More events in js

--- leccion06/leccion06.js
+++ leccion06/leccion06.js
@@ -23,3 +23,59 @@ for ( let i = 0; i < autos.length; i++){
     console.log(i + " : " + autos[i]);
    
 }
+console.log(".......................................");
+//Delete the last element of the array
+const frutas = ['manzana', 'pera', 'plátano'];
+const ultimoElemento = frutas.pop();
+console.log(ultimoElemento); // Result: 'plátano'
+console.log(frutas);
+
+console.log(".......................................");
+//Delete the first one 
+const frutas2 = ['manzana', 'pera', 'plátano'];
+const primerElemento = frutas2.shift();
+console.log(primerElemento); // Resultado: 'manzana'
+console.log(frutas2);
+
+//Add elements in the first place 
+frutas2.unshift('manzana', 'perro');
+console.log(frutas2); // Resultado: ['plátano', 'manzana', 'pera']
+
+console.log(".......................................");
+
+//Conbine two arrays and makes a new one 
+const array1 = [1, 2];
+const array2 = [3, 4];
+const combinado = array1.concat(array2);
+console.log(combinado); // Resultado: [1, 2, 3, 4]
+
+console.log(".......................................");
+
+const frutas3 = ['manzana', 'pera', 'plátano', 'naranja'];
+frutas3.splice(2, 1, 'uva'); //The 1 indicates how many elements will be deleted
+console.log(frutas3); // Resultado: ['manzana', 'pera', 'uva', 'naranja']
+
+
+console.log(".......................................");
+
+//For each element in the array fruta will started
+const frutas4 = ['manzana', 'pera', 'plátano'];
+frutas4.forEach(fruta => console.log(fruta));
+
+console.log("Other for");
+for(let i = 0; frutas4.length > i; i++){
+    
+    console.log(frutas4[i]);
+
+}
+
+console.log(".......................................");
+
+const numeros = [1, 2, 3];
+const duplicados = numeros.map(numero => numero * 2);
+console.log(duplicados); // Resultado: [2, 4, 6]
+
+console.log(".......................................");
+const numeros2 = [1, 2, 3, 4, 5];
+const pares = numeros2.filter(numero => numero % 2 === 0);
+console.log(pares); // Resultado: [2, 4]
--- leccion11/leccion11.js
+++ leccion11/leccion11.js
@@ -1,14 +1,21 @@
 // Events give dynamism to the page 
 // onClick event
+//DOMContentLoaded this is like a onload because this event start when the DOM is complete loaded
 document.addEventListener("DOMContentLoaded" , function(){
 
     var boton = document.getElementById("onclick");
     var componente = document.getElementById("title");
-    
-    console.log(componente.textContent);
+    var pTitulo = componente.textContent;
 
     function evento(a){
-        componente.textContent = a;
+        
+
+        if( componente.textContent === a){
+            componente.textContent = pTitulo;
+        }
+        else{
+            componente.textContent = a;
+        }
     }
     
     boton.addEventListener('click' , function(){ //This structure causes the "Click" instruction to be done first and then the function
@@ -17,6 +24,118 @@ document.addEventListener("DOMContentLoaded" , function(){
 })
 
 
+// onLoad event 
+//it waits to load all resources of the page, ALL, the dom , images , etc. It is slower than DOMContentLoaded
+window.onload = function() {
+    var boton2 = document.getElementById("onclick");
+    var componente2 = document.getElementById("title");
+    var pTitulo2 = componente2.textContent;
+
+    function evento2(a) {
+        if (componente2.textContent === a) {
+            componente2.textContent = pTitulo2;
+        } else {
+            componente2.textContent = a;
+        }
+    }
+
+    boton2.addEventListener('click', function() {
+        evento2("asdf");
+    });
+};
+
+//onChange
+document.addEventListener("DOMContentLoaded" , function(){
+    
+    var selector = document.getElementById("selector");
+    var mostrarSelector = document.getElementById("mostrarSelector");
+    
+    console.log(selector.value); //Gives the first option
+    console.log(selector.textContent); //Gives all the options 
+
+    mostrarSelector.textContent = "Opción seleccionada: " + selector.value;
+
+    selector.addEventListener("change", function() {
+
+        var opcionSeleccionada = selector.value;
+        
+        console.log(opcionSeleccionada);
+
+        mostrarSelector.textContent = "Opción seleccionada: " + opcionSeleccionada;
+        
+    })
+   
+
+})
+
+
+//onMouseOver , onMouseOut
+document.addEventListener("DOMContentLoaded" , function(){
+
+    var titulo = document.getElementById("onMouse");
+
+    titulo.addEventListener("mouseover" , function(){
+        titulo.style.color = "red";
+    })
+
+    titulo.addEventListener("mouseout" , function(){
+        titulo.style.color = "black";
+    })
+
+
+})
+
+
+//onMouseDown , onMouseUp
+document.addEventListener("DOMContentLoaded" , function(){
+
+    var titulo = document.getElementById("onMouseDown");
+
+    titulo.addEventListener("mousedown" , function(){
+        titulo.style.color = "red";
+    })
+
+    titulo.addEventListener("mouseup" , function(){
+        titulo.style.color = "black";
+    })
+
+
+})
+
+
+//onFocus , onBlur
+document.addEventListener("DOMContentLoaded" , function(){
+
+    var input = document.getElementById("onFocusBlur");
+
+    input.addEventListener("focus" , function(){
+        input.style.backgroundColor = "red";
+    })
+
+    input.addEventListener("blur" , function(){
+        input.style.backgroundColor = "white";
+    })
+
+
+})
+
+
+
+//adEventListener
+document.addEventListener("DOMContentLoaded" , function(){
+
+    var input = document.getElementById("onFocusBlur");
+
+    input.addEventListener("focus" , function(){
+        input.style.backgroundColor = "red";
+    })
+
+    input.addEventListener("blur" , function(){
+        input.style.backgroundColor = "white";
+    })
+
+
+})
 
     
 
